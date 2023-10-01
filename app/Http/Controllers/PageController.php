4fe101d9e52c4fa545798cce630335263fef3570
kotaiba <kotaiba@acornassociated.org>
2023-10-01 10:05:24 +0300

    dashboard blog

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index(){
+        $posts = Post::where('featured', false)
+                    ->with('user', 'categories')
+                    ->get();
+        $featured = Post::featured()->take(3)->get();
+        // dd($featured);
+        return view('front.index', [
+            'posts' => $posts,
+            'featured' => $featured
+        ]);
+    }
+
+    public function posts(){
+        return view('posts.index');
+    }
+    
+    public function showPost(Post $post){
+        $post = $post->load('user','categories');
+        return view('front.posts.show', compact('post'));
+    }
+
+    public function showCategory(Category $category){
+        $posts = $category->posts()->get();
+        return view('front.categories.show', compact('category', 'posts'));
+    }
+}
---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\{Category, Post};
+use App\Observers\PostObserver;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+        $categories = Category::all();
+        View::composer('*', function ($view) use ($categories) {
+            $view->with('categories', $categories);
+        });
+        
+        Post::observe(PostObserver::class);
+    }
+}
